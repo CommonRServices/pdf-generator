d8c9e6ec14f451e2ae9ed12ea767083ccefe4839
DamyanBG <d.dimitrov.1988@abv.bg>
2026-02-08 15:17:02 +0200

    Implement browser lifecycle management and PDF generation endpoint

--- app/main.py
+++ app/main.py
@@ -3,3 +3,22 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from playwright.async_api import async_playwright
 
+from app.router import router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    playwright = await async_playwright().start()
+    browser = await playwright.chromium.launch()
+
+    app.state.browser = browser
+
+    yield
+
+    await browser.close()
+    await playwright.stop()
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router)
--- app/router.py
+++ app/router.py
@@ -2,4 +2,15 @@ from fastapi import APIRouter, Request
 from fastapi.responses import Response
 from playwright.async_api import Browser
 
-from 
+from app.schemas import GeneratePdfRequest
+from app.service import generate_pdf_bytes
+
+router = APIRouter(prefix="/pdf-generator")
+
+
+@router.post("/generate-pdf")
+async def generate_pdf(request: Request, payload: GeneratePdfRequest) -> Response:
+    browser: Browser = request.app.state.browser
+    pdf_bytes = await generate_pdf_bytes(payload, browser)
+
+    return Response(content=pdf_bytes, media_type="application/pdf")

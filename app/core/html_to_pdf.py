3cdd723c558f2a432baea77fc979c84b6620d8a2
DamyanBG <d.dimitrov.1988@abv.bg>
2026-02-16 15:10:14 +0200

    Refactor PDF generation to create a new browser context for improved resource management

--- app/core/html_to_pdf.py
+++ app/core/html_to_pdf.py
@@ -2,7 +2,9 @@ from playwright.async_api import Browser
 
 
 async def html_string_to_pdf(html_content: str, output_pdf_path: str, browser: Browser):
-    page = await browser.new_page()
+    context = await browser.new_context()
+    page = await context.new_page()
     await page.set_content(html_content, wait_until="load")
     await page.pdf(path=output_pdf_path, format="A4", print_background=True)
     await page.close()
+    await context.close()
